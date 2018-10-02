f864292b1f7182e0943f00a80b0a5e9cf23bdcb3
Edgar <edgaramiroff@gmail.com>
2018-10-02 20:13:56 +0400

    Reverse a string

--- 12th_problem.js
+++ 12th_problem.js
@@ -1,7 +1,27 @@
 // Reverse a string
 
-some_str = 'Hello, world !!!'
+let some_str = 'Hello';
 
-let start, end, tmp;
-let str_len = some_str.length
+// Place algorithm in a function
 
+function reverse_string(string_item) {
+
+// Define start and end of the string and temporary variable
+// for keeping the first character
+    let start = 0, end = string_item.length - 1;
+    let tmp;
+// Make string an array to avoid problems with string
+    string_item = string_item.split('');
+    while (start<end) {
+        tmp = string_item[start];
+        string_item[start]=string_item[end];
+        string_item[end] = tmp;
+        start++;
+        end--;
+        }
+// Make array back to string
+    string_item = string_item.join('');
+    console.log(string_item);
+}
+
+reverse_string(some_str)
